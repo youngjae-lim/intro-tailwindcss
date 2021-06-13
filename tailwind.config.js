2aba68a348a582e1d7aa2cc4591750c7caa5ec9b
Youngjae Lim <limyoungjae@gmail.com>
2021-06-12 20:18:41 -0400

    Added responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'xs': '268px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   variants: {
